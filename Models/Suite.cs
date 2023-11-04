0582e7406cd0ab6b04562d0edbbf8cccae9cb9bf
Oaní da Silva da Costa <oani_costa@outlook.com.br>
2023-11-03 21:23:31 -0300

    Desafio Concluido

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -2,12 +2,16 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Suite
     {
-        public Suite() { }
+        public Suite()
+        { }
 
         public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
         {
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
+
+            if (valorDiaria < 0)
+                throw new Exception("Valor da diaria não pode ser negativo");
             ValorDiaria = valorDiaria;
         }
 
